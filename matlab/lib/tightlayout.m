4fafc3a96951fe8cc286a122f1ed8a22f6f0f10f
Richard Foltyn <richard.foltyn@gmail.com>
2021-02-08 23:37:18 +0100

    Matlab: improve algorithm for tightlayout()

--- matlab/lib/tightlayout.m
+++ matlab/lib/tightlayout.m
@@ -1,5 +1,5 @@
 
-function tightlayout(hfig,layout,papersize)
+function tightlayout(hfig,layout,add_margins,papersize)
 %   TIGHTLAYOUT removes excessive margins around subplots.
 %
 %   TIGHTLAYOUT(HFIG) removes excessive margins for the given figure
@@ -9,23 +9,36 @@ function tightlayout(hfig,layout,papersize)
 %           figure, assuming that subplots are arranged using the
 %           LAYOUT = [NROW, NCOL].
 %
-%   TIGHTLAYOUT(HFIG,NROW,NCOL,PAPERSIZE) removes excessive margins
-%           for the given figure, assuming that subplots are arranged 
-%           using the LAYOUT = [NROW, NCOL]. Additionally, the paper size
-%           is set to the given [width, height] values (only relevant when
-%           printing or saving).
+%   TIGHTLAYOUT(HFIG,LAYOUT,ADD_MARGINS) additionally increases the margins
+%           of each subplot by the values given in the array 
+%           ADD_MARGINS = [LEFT, BOTTOM, RIGHT, TOP]. Alternatively, ADD_MARGINS
+%           can be a scaler if the same amount should be added to all sides.
+%
+%   TIGHTLAYOUT(HFIG,NROW,NCOL,ADD_MARGINS,PAPERSIZE) Additionally, sets 
+%           the paper size to the given [width, height] values (only relevant 
+%           when printing or saving).
+
+    DEFAULT_ADD_MARGINS = repmat(0.1, [1 4]);
 
     switch nargin
         case 0
             hfig = gcf();
         case 1
             layout = [1 1];
+            add_margins = DEFAULT_ADD_MARGINS
         case 2
-            % Admissible number of args, no action needed
+            add_margins = DEFAULT_ADD_MARGINS
         case 3
+            papersize = [];
+        case 4
             % Admissible number of args, no action needed
         otherwise
             error('Unsupported number of arguments: %d', nargin);
+    end
+    
+    % Replicate scalar margin across all four sides
+    if isscalar(add_margins)
+        add_margins = repmat(add_margins, [1 4]);
     end
     
     nrow = layout(1);
@@ -37,55 +50,42 @@ function tightlayout(hfig,layout,papersize)
 
     naxes = length(axes);       % total number of axes in the figure
     
-    % compute harmonized margins (left,bottom,top,right) across all
+    % compute harmonized margins (left,bottom,right,top) across all
     % subplots. For this we find the max. for each margin.
-    dleft = 0.0;
-    dbottom = 0.0;
-    dright = 0.0;
-    dtop = 0.0;
+    margins = zeros([1 4]);
     
-    % Positioning of axes within figure seems to work somewhat differently
-    % in Octave, skip as it does not yield the desired results.
-    if ~isOctave()
-        for k = 1:naxes
-            hax = axes(k);
-            % This does not work in Octave!
-            set(hax, 'XLimSpec', 'Tight', 'YLimSpec', 'Tight');
-            
-            outer = get(hax, 'OuterPosition');
-            inner = get(hax, 'Position');
-            diff = inner - outer;
-            
-            dleft = max(dleft, diff(1));
-            dbottom = max(dbottom, diff(2));
-            % Max. right margin: 
-            % (outer(1) + outer(3)) - (inner(1) + inner(3))
-            %   = (outer(1) - inner(1)) + (outer(3) - inner(3))
-            dright = max(dright, -diff(1) - diff(3));
-            % Max. top margin: 
-            % (1 - outer(2) - outer(4)) - (1 - inner(2) - inner(4)) 
-            %   = (inner(2) - outer(2)) + (inner(4) - outer(4))
-            dtop = max(dtop, diff(2) + diff(4));
-        end
+    for k = 0:naxes-1
+        hax = axes(naxes-k);
         
-        % Apply position to each subplot
-        for k = 0:naxes-1
-            i = floor(k/ncol) + 1;
-            j = rem(k, ncol) + 1;
-            
-            hax = axes(naxes-k);
-            
-            left = (j - 1)/ncol + dleft;
-            bottom = (nrow - i)/nrow + dbottom;
-            width = 1.0/ncol - dleft - dright;
-            height = 1.0/nrow - dtop - dbottom;
-            pos = [left bottom width height];
-            set(hax, 'activepositionproperty', 'position');
-            set(hax, 'Position', pos);
+        % This does not work in Octave!
+        if ~isOctave()
+            set(hax, 'XLimSpec', 'Tight', 'YLimSpec', 'Tight');
         end
+        inset = get(hax, 'TightInset');
+        
+        margins = max(margins, inset);
     end
     
-    if nargin == 3
+    % short-hand to have less cluttered expressions
+    am = add_margins;
+    
+    % Apply position to each subplot
+    for k = 0:naxes-1
+        i = floor(k/ncol) + 1;
+        j = rem(k, ncol) + 1;
+        
+        hax = axes(naxes-k);
+        
+        left = (j - 1)/ncol + margins(1) + am(1);
+        bottom = (nrow - i)/nrow + margins(2) + am(2);
+        width = 1.0/ncol - (margins(1) + margins(3) + am(1) + am(3));
+        height = 1.0/nrow - (margins(2) + margins(4) + am(2) + am(4));
+        pos = [left bottom width height];
+        set(hax, 'activepositionproperty', 'position');
+        set(hax, 'Position', pos);
+    end
+    
+    if size(papersize) > 0
         % set PaperSize if argument is present.
         set(hfig, 'PaperUnits', 'inches');
         set(hfig, 'PaperSize', papersize);
