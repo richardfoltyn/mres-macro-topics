62d4caee5d6f2f739d28617ebea15e6a51d2f039
Richard Foltyn <richard.foltyn@gmail.com>
2021-02-09 19:37:46 +0100

    MATLAB/EGM: Add initial implementation

---
+++ matlab/EGM/egm_IH.m
@@ -0,0 +1,131 @@
+
+function [pfun_cons, pfun_sav] = egm_IH(par, tol, maxiter)
+% EGM_IH   Solve the infinite-horizon HH problem using EGM.
+%
+%   [PFUN_CONS, PFUN_SAV] = EGM_IH(PAR) returns the consumption and savings
+%       policy functions for the problem parametrised by PAR.
+%
+%   [PFUN_CONS, PFUN_SAV] = EGM_IH(PAR,TOL,MAXITER) returns the consumption and 
+%       savings policy functions for the problem parametrised by PAR,
+%       using the termination tolerance TOL and a max. number of iterations
+%       given by MAXITER.
+%
+%   Note: This implementation uses the same grid for exogenous savings
+%       and beginning-of-period assets. This is not required, but spares
+%       is unnecessary interpolation steps.
+%
+% Author: Richard Foltyn
+
+    % Set default values for optional arguments
+    switch nargin
+        case 1
+            % Only PAR passed, assume default TOL and MAXITER
+            tol = 1.0e-6;
+            maxiter = 1000;
+        case 2
+            % Two arguments passed, assume default MAXITER
+            maxiter = 1000;
+        case 3
+            % All arguments present
+        otherwise
+            error('Invalid number of arguments: %d', nargin);
+    end
+
+  
+    % start timer to calculate how long it takes to run VFI
+    tstart = tic;
+    
+    % Check whether we have a problem with income risk. In this case we 
+    % assume that the field GRID_Y and TM_Y are fields in the structure
+    % PAR and store the states and transition matrix of income shocks.
+    % If these fields are not present, we assume that income is
+    % deterministic and equal to 1.0 each period.
+    
+    if isfield(par, 'grid_y') && isfield(par, 'tm_y')
+        grid_y = par.grid_y;
+        tm_y = par.tm_y;
+        N_y = length(par.grid_y);
+    else
+        grid_y = ones(1,1);     % degenerate income state vector with 1 element
+        tm_y = ones(1,1);       % degenerate 1-by-1 transition matrix
+        N_y = 1;
+    end
+
+    N_a = length(par.grid_a);
+
+    % Precompute cash-at-hand for each asset/savings grid point
+    % so we don't have to do that repeatedly in each loop iteration.
+    cah = (1.0 + par.r) * par.grid_a + grid_y';
+    
+    % Initialize arrays to hold optional consumption as a function of 
+    % savings brought over from previous period.
+    
+    % Initial guess for consumption policy: consume entire cash-at-hand
+    pfun_cons = cah;
+    pfun_cons_upd = NaN([N_a, N_y]);
+  
+    for iter = 1:maxiter
+    
+        % Iterate over income levels
+        for iy = 1:N_y
+               
+            % Expected marginal utility tomorrow as a function of
+            % savings today (which are tomorrow's assets).
+            mu = pfun_cons.^(-par.gamma) * tm_y(iy,:)';
+
+            % Compute right-hand side of Euler equation
+            ee_rhs = par.beta * (1.0 + par.r) * mu;
+
+            % Invert Euler equation to get optimal consumption today as
+            % a function of today's savings.
+            cons_sav = ee_rhs.^(-1.0/par.gamma);
+
+            % Use budget constraint to get beginning-of-period assets
+            % as a function of today's savings.
+            assets_sav = (cons_sav + par.grid_a - grid_y(iy)) / (1.0 + par.r);
+
+            % Interpolate results back onto exogeneous asset/savings grid.
+            pfun_cons_upd(:,iy) = interp1(assets_sav, cons_sav, ...
+                par.grid_a, 'linear', 'extrap');
+
+            % Fix consumption policy for states in which HH does not
+            % save anything.
+            ia = find(par.grid_a < assets_sav(1));
+            if size(ia) > 0
+                pfun_cons_upd(ia,iy) = cah(ia,iy);
+            end
+        end
+        
+        % Make sure that consumption policy satisfies constraints
+        assert(all(all(pfun_cons_upd >= 0.0)) && all(all(pfun_cons_upd <= cah)));
+        
+        % check whether we have convergence, ie. difference to last iteration is
+        % below desired tolerance level. If this is the case, exit the function,
+        % otherwise proceed with next iteration.
+        diff = max(max(abs(pfun_cons - pfun_cons_upd)));
+        
+        % update using newly computed value function
+        pfun_cons = pfun_cons_upd;
+
+        % Update current guess for savings policy function defined on
+        % beginning-of-period assets grid.
+        pfun_sav = cah - pfun_cons;
+          
+        if diff < tol
+            % Desired tolerance level achieved, terminate iteration.
+            tend = toc(tstart);
+            fprintf("EGM: Converged after %d iterations in %4.1f sec.; dC=%.2e\n", ...
+                iter, tend, diff);
+            return;
+        else
+            % print progress in the first iteration, and thereafter every 10 
+            % iterations.
+            if (mod(iter, 10) == 0) || (iter == 1)
+                fprintf("EGM: iteration %3d; dC = %.2e\n", iter, diff);
+            end
+        end
+    end
+  
+    warning("EGM: Exceeded max number of iterations; dC=%.2e\n", diff);
+
+end
--- matlab/EGM/main_labour_risk.m
+++ matlab/EGM/main_labour_risk.m
@@ -1,8 +1,8 @@
 %
 % Topics in Macroeconomics (ECON5098), 2020-21
 %
-% Main file to run value function iteration (VFI) for problem with 
-% risky labour income and plot results.
+% Main file to run the infinite-horizon endogenous grid-point method (EGM) 
+% for problem with risky labour income and plot results.
 %
 % Author: Richard Foltyn
 
@@ -23,10 +23,10 @@ close all
 par.beta = 0.96;            % Discount factor
 par.gamma = 2.0;            % Relative risk aversion (RRA)
 par.r = 0.04;               % Interest rate
+par.y = 1;                  % Wage rate
 
-% Asset grid parameters
 par.a_max = 50;             % Upper bound of asset grid
-par.N_a = 50;               % Number of points on asset grid
+par.N_a = 100;              % Number of points on asset grid
 
 % Labour income process parameters
 par.rho = 0.95;             % Persistence of AR(1)
@@ -57,8 +57,7 @@ grid_y = states / (edist' * states);
 par.grid_y = grid_y;
 par.tm_y = tm_y;
 
-
-%% Run value function iteration
+%% Run EGM
 
 % Termination tolerance for VFI
 tol = 1.0e-6;
@@ -66,23 +65,9 @@ tol = 1.0e-6;
 maxiter = 1000;
 
 % Solve problem using grid search
-[vfun, pfun_ia] = vfi_risk(par, tol, maxiter);
-% Optimal next-period asset level (savings)
-a_opt = par.grid_a(pfun_ia);
+[cons_opt, a_opt] = egm_IH(par, tol, maxiter);
 
-% Solve problem using interpolation. Admissible interpolation methods
-% are those accepted by the interp1() function, e.g. 'linear', 'cubic',
-% 'pchip', 'spline', ...
-% [vfun, a_opt] = vfi_risk_interp(par, tol, maxiter, 'pchip');
-
-
-%% Plot value and policy functions for savings and consumption
-
-% Cash-at-hand at beginning of period
-cah = (1.0 + par.r) * par.grid_a + par.grid_y';
-
-% Optimal consumption level
-cons_opt = cah - a_opt;
+%% Plot policy functions for savings and consumption
 
 % Settings governing plot style
 aspect = 1.0;
@@ -110,25 +95,8 @@ colours = [low' mid' high'];
 % Legend labels
 labels = arrayfun(@(x) sprintf('y=%.3f', x), par.grid_y, 'UniformOutput', false);
 
-% Plot value functions
-subplot(1,3,1);
-for iy = 1:par.N_y
-    plot(par.grid_a, vfun(:,iy), 'LineWidth', 1.5, 'Color', colours(:,iy));
-    hold on;
-end
-hold off;
-% Use custom function to set common plot style
-set_ax_plot_style(gca, aspect);
-title('Value function');
-xlabel('Assets');
-% Set plot limits and ticks
-xlim(xlim_);
-ylim([min(vfun(1,:)), max(vfun(ixmax,:))]);
-xticks(xticks_);
-legend(labels, 'Location', 'SouthEast');
-
 % Plot savings (i.e. next-period assets)
-subplot(1,3,2);
+subplot(1,2,1);
 for iy = 1:par.N_y
     plot(par.grid_a, a_opt(:,iy), 'LineWidth', 1.5, 'Color', colours(:,iy));
     hold on;
@@ -142,9 +110,10 @@ xlabel('Assets');
 xlim(xlim_);
 ylim([0, max(a_opt(ixmax,:))]);
 xticks(xticks_);
+legend(labels, 'Location', 'NorthWest');
 
 % Plot optimal consumption
-subplot(1,3,3);
+subplot(1,2,2);
 for iy = 1:par.N_y
     plot(par.grid_a, cons_opt(:,iy), 'LineWidth', 1.5, 'Color', colours(:,iy));
     hold on;
@@ -167,19 +136,12 @@ graphdir = '../graphs';
 mkdir(graphdir)
 
 % Custom function to remove excessive margins around subplots
-papersize = [8.5 3.0];
-layout = [1 3];
+papersize = [6.0 3.0];
+layout = [1 2];
 tightlayout(gcf, layout, 0.02, papersize);
 
 % Store graphs as PDF, add number of asset grid points to file name.
-if exist('pfun_ia', 'var') ~= 0
-    % Array pfun_ia is defined, so solution was obtained from grid search
-    fn = sprintf('VFI_labour_risk_N%d.pdf', par.N_a);
-else
-    % Solution was obtained using interpolation, append suffix
-    fn = sprintf('VFI_labour_risk_N%d_interp.pdf', par.N_a);
-end
-
+fn = sprintf('EGM_labour_risk_N%d.pdf', par.N_a);
 fn = fullfile(graphdir, fn);
 saveas(gcf, fn)
 
